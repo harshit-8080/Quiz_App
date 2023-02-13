bbddbd019eb0cf9e2e0876428cea592d03460a10
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2023-02-13 21:14:32 +0530

    question widget added

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: unused_local_variable
+
+import "package:flutter/material.dart";
+import './question.dart';
+
+// void main () => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppStateless();
+  }
+}
+
+class _MyAppStateless extends State<MyApp> {
+  int _index = 0;
+  void _answerQuestion() {
+    setState(() {
+      _index = _index + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const _questions = [
+      "What is fav color ?",
+      "What is fav animal ?",
+      "What is fav player ?",
+      "What is fav game ?",
+      "What is fav city ?"
+    ];
+    // constructor calling and named arugments
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: Text("My First App")),
+            body: Column(children: [
+              Question(_questions[_index]),
+              ElevatedButton(
+                  child: Text("Answer 1"), onPressed: _answerQuestion),
+              ElevatedButton(
+                  child: Text("Answer 2"), onPressed: _answerQuestion),
+              ElevatedButton(
+                  child: Text("Answer 3"), onPressed: _answerQuestion),
+            ])));
+  }
+}
